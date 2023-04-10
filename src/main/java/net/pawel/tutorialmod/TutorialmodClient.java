812b715a898814f7ffb4099d06851f2f97151b29
Kaaramba <paw.szew1234@gmail.com>
2023-04-10 09:51:15 +0200

    eeee

---
+++ src/main/java/net/pawel/tutorialmod/TutorialmodClient.java
@@ -0,0 +1,10 @@
+package net.pawel.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
